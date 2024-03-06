9c3ee64888db26a62770eb75b66ca602391ac074
mmolalekan <muhayminolalekan@yahoo.com>
2024-03-06 12:23:28 +0100

    project 0x01

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      const result = [];
+      for (const item of values) {
+        result.push({ status: item.status, value: item.value || item.reason });
+      }
+    });
+}
