6e5d33f2584d3b3812a7dc870fb20fb834e0d0a7
mmolalekan <muhayminolalekan@yahoo.com>
2024-03-08 01:32:08 +0100

    task 6

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -5,10 +5,10 @@ export default class Building {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
-    this._sqft = sqft;
+    this._sqft = sqft; // eslint-disable-line no-underscore-dangle
   }
 
   get sqft() {
-    return this._sqft;
+    return this._sqft; // eslint-disable-line no-underscore-dangle
   }
 }
