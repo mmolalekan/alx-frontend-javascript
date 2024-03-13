57496a7ea4a6dd50e86557515e3bc482061591dc
mmolalekan <muhayminolalekan@yahoo.com>
2024-03-13 17:45:17 +0100

    0x03 corrections

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,16 @@
 export default function cleanSet(set, startString) {
+  const parts = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
 
-  const myArray = Array.from(set).filter((item) => item.startsWith(startString));
-  const newArray = myArray.map((item) => item.slice(startString.length));
-  return newArray.join('-');
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
